e6bd04a28197a167dc59d4cb4ed7a1371170fba8
chris@vendiadvertising.com <chris@vendiadvertising.com>
2017-03-01 09:34:26 -0600

    Added support for parsing phpinfo

--- qth-site-builder/host_info.php
+++ qth-site-builder/host_info.php
@@ -16,23 +16,86 @@ class host_info extends base_plugin
         $local_key_name = 'qth_server_name';
 
         $hostname = $this->cache_get( $local_key_name );
+
         if( ! $hostname )
         {
-            $hostname = $this->get_hostname();
+            $hostname = $this->get_qth_server_name_from_env();
+
+            if( ! $hostname )
+            {
+                $hostname = $this->get_qth_server_name_from_phpinfo();
+            }
 
             if( $hostname )
             {
-                $parts = explode( '.', $hostname );
-                if( 3 === count( $parts ) && 'website-server' === $parts[ 1 ] && 'net' === $parts[ 2 ] )
-                {
-                    $hostname = 'QTH Server #' . str_replace( 'www', '', $parts[ 0 ] );
-                }
-
                 $this->cache_set( $local_key_name, $hostname );
             }
-            else
+        }
+
+        if( ! $hostname )
+        {
+            $hostname = 'Could not determine server&rsquo;s host';
+        }
+
+        return $hostname;
+    }
+
+    public function try_get_server_number_from_string( $value )
+    {
+        $parts = explode( '.', $value );
+        if( 3 === count( $parts ) && 'website-server' === $parts[ 1 ] && 'net' === $parts[ 2 ] )
+        {
+            return (int) str_replace( 'www', '', $parts[ 0 ] );
+        }
+
+        return false;
+    }
+
+    public function convert_server_number_to_string( $server_number )
+    {
+        return 'QTH Server #' . $server_number;
+    }
+
+    public function get_qth_server_name_from_phpinfo()
+    {
+        $subject = '<tr><td class="e">System </td><td class="v">Linux www15.website-server.net 2.6.32-673.26.1.lve1.4.18.el6.x86_64 #1 SMP Fri Oct 21 11:58:14 EDT 2016 x86_64 </td></tr>';
+        $pattern = preg_quote( '<tr><td class="e">System </td><td class="v">' ) .
+                    '(?<value>[^<]*)' .
+                    preg_quote( '</td></tr>' );
+        $count = preg_match( '~' . $pattern . '~', $subject, $matches );
+
+        if( $count && array_key_exists( 'value', $matches ) )
+        {
+            foreach( explode( ' ', $matches[ 'value' ] ) as $string )
             {
-                $hostname = 'Could not determine server&rsquo;s host';
+                $server_number = $this->try_get_server_number_from_string( $string );
+                if( $server_number )
+                {
+                    $hostname = $this->convert_server_number_to_string( $server_number );
+                    return $hostname;
+                }
+            }
+        }
+
+        return false;
+
+        // return false;
+
+        // dump( $matches );
+        // die;
+    }
+
+    public function get_qth_server_name_from_env()
+    {
+
+        $hostname = $this->get_hostname();
+
+        if( $hostname )
+        {
+            $server_number = $this->try_get_server_number_from_string( $hostname );
+            if( $server_number )
+            {
+                $hostname = $this->convert_server_number_to_string( $server_number );
             }
         }
 
